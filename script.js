4ca8b33438e9482263d1f2916cbe31f11cecf6ab
Noel Tesfalidet <58369280+NoelMT@users.noreply.github.com>
2022-04-13 16:54:11 +0200

    weather

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 
 
-function loop() {
-    setTimeout(function() {
-        var today = new Date();
-        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        document.getElementById("time").innerHTML = time
-        loop();
-    }, 150)
+ let url = "https://api.openweathermap.org/data/2.5/weather?q="
+ let city ="Stockholm"
+ let apikey =  "&appid=be0211d09b3b1c062b61ffe1f18c49d4&units=metric"
+
+
+function getwether(city) {
+  fetch(
+    url+city+apikey
+  ).then((response) => response.json()).then(
+
+    (data) => this.display(data)
+  )
 }
 
-loop();
+function display(data){
+  const { name } = data;
+  const { icon, description } = data.weather[0];
+  const { temp } = data.main;
+
+  document.querySelector(".city").innerHTML =  name;
+  document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+  document.querySelector(".describe").innerText = description;
+  document.querySelector(".temp").innerText = temp + "°C";
+
+}
+
+document.querySelector(".bar").addEventListener("keypress", function(event){
+if(event.key == "Enter")
+search();
+})
+
+
+function search(){
+  this.getwether(document.querySelector(".bar").value);
+}
+document.querySelector(".search button").addEventListener("click", function () {
+  search();
+});
